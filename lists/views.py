5b816fc11a8614aac232f255d12294a39bb4f84d
isabellevieirab <ivbisabelle@gmail.com>
2022-10-25 22:40:20 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']

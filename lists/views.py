a5c8f38644fb39accaea0ef9724de0b7bb54d02a
isabellevieirab <ivbisabelle@gmail.com>
2022-10-25 23:59:58 -0300

    Novo Projeto: Atualizando o Template

--- lists/views.py
+++ lists/views.py
@@ -3,8 +3,7 @@ from lists.models import Item, List
 
 def view_list(request, list_id):
     my_list = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=my_list)
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'list': my_list})
 
 def home_page(request):
     return render(request, 'home.html')

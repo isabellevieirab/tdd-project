28902f64074bef0187ca14895dd901b083d83085
isabellevieirab <ivbisabelle@gmail.com>
2022-11-29 21:41:17 -0300

    Layout e o estilo da aplicação

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
